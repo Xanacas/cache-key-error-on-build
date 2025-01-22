ca26418ebbc2f21a807c556484c45c7214f13fa7
Xanacas <53140620+Xanacas@users.noreply.github.com>
2025-01-22 13:12:57 +0100

    more minimal

--- data-access-layer/projects.ts
+++
@@ -1,9 +0,0 @@
-export async function getLatestProjects() {
-    "use cache";
-
-    /* In real life, this would be a call to a database */
-    return [
-        { id: "1", name: "Project 1" },
-        { id: "2", name: "Project 2" },
-    ];
-}
